336addaa8de139cc62fbe005e41785f07637a514
Muhammad Ayan Hashmi <ahashmi5401@gmail.com>
2026-05-25 03:11:19 +0500

    Method for dynamic render satic page is >>   {dynami ='force-dynamic'} >> 2nd method {use searchParams } >> 3rd method {use cookies by import from next/header and also use await bc it return promise like searchParams} >>

--- how-to-forcely-apply-dynamic-rerendering-on-static-page/app/page.tsx
+++ how-to-forcely-apply-dynamic-rerendering-on-static-page/app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import { cookies } from 'next/headers'
 const dynamic = 'force-dynamic'; // Force dynamic rendering for this page
-const page = () => {
-  return (
+
+//2nd methodd how we forcefully do dyynamic rendring on satis page  by using searchParams
+const page = async ({searchParams} : any) => {
+  const search = await  searchParams
+
+  //3rd method  how we forcefully do dyynamic rendring on satis page 
+  const cooky = await cookies()
+return (
     <div>Muhammad Ayan Hashmi</div>
   )
 }
