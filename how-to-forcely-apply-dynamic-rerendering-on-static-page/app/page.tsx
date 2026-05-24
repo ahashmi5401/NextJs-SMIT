59ccd2f2f838c2e77c5f70221ae0e48960e50e57
Muhammad Ayan Hashmi <ahashmi5401@gmail.com>
2026-05-25 03:01:53 +0500

    use this in the page for forecely dyanmic rendering {const dynamic = 'force-dynamic'}

---
+++ how-to-forcely-apply-dynamic-rerendering-on-static-page/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const dynamic = 'force-dynamic'; // Force dynamic rendering for this page
+const page = () => {
+  return (
+    <div>Muhammad Ayan Hashmi</div>
+  )
+}
+
+export default page
